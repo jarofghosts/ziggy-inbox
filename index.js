a8be2c025fcaff2c9e2186f1dd0b6c62e6b8958f
jesse keane <me@jessekeane.me>
2014-03-07 17:05:54 -0800

    bleh

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ function inbox(ziggy) {
   ziggy.on('join', check_inbox)
 
   function parse_message(user, channel, message) {
+    var bits = message.split(/\s+/)
+
+    var command = bits[0]
+      , rest = bits.slice(1).join(' ')
+
+    if(command !== '!tell' && command !== '!inbox') return
+
   }
 
   function parse_pm(user, message) {
@@ -16,5 +23,18 @@ function inbox(ziggy) {
   }
 
   function check_inbox(user, channel) {
+    db.get(user.nick, show_inbox)
+
+    function show_inbox(err, _messages) {
+      if(err) {
+        return say_nothing(channel)
+      }
+
+      var message = JSON.parse(_messages)
+
+      for(var i = 0, l = messages.length; i < l; ++i) {
+        ziggy.say(channel, messages[i])
+      }
+    }
   }
 }
